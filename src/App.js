ff83b2a7ebc2a42c9a6589d891216695fd051036
Israel <93683580+blueishegg@users.noreply.github.com>
2022-11-07 09:56:06 +0000

    react app created and ran, simple edits made to test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import isra from "./isra.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={isra} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CALDERON WEBSITE
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
