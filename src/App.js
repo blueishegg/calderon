b5efe933d348a07b50235568cc012a21b3096f4b
Israel <93683580+blueishegg@users.noreply.github.com>
2022-11-19 19:50:48 +0000

    connecting to mongodb with env variables

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import isra from "./isra.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
+import * as Realm from "realm-web";
 
 function App() {
-  const [data, setData] = useState();
-  const fetchData = async function () {
-    const response = await fetch("http://localhost:1234/");
-    setData(await response.json());
-  };
+  //-----CONNECTING TO MONGODB----
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
+    async function fetchData() {
+      const REALM_APP_ID = process.env.REACT_APP_CALDERON_MONGO;
+      const app = new Realm.App({ id: REALM_APP_ID });
+      const credentials = Realm.Credentials.anonymous();
+      try {
+        const user = await app.logIn(credentials);
+        const allUsers = await user.functions.getAllUsers();
+        setUsers(allUsers);
+      } catch (error) {
+        console.error(error);
+      }
+    }
     fetchData();
   }, []);
-  console.log(data.payload[1].forename);
+
+  // const [data, setData] = useState();
+  // const fetchData = async function () {
+  //   const response = await fetch("http://localhost:1234/");
+  //   setData(await response.json());
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+  // console.log(data.payload[1].forename);
   // console.log("-----HELLO-----");
   // fetch("http://localhost:1234/")
   //   .then((res) => res.json())
@@ -33,6 +53,12 @@ function App() {
           calderon
         </a>
       </header>
+      <div>
+        {users &&
+          users.map((user) => {
+            return <p key={users._id}>{user.full_name}</p>;
+          })}
+      </div>
     </div>
   );
 }
