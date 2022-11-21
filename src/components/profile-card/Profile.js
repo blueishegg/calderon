246dab17fbbea89606af9fa782e6fa505895ef12
Israel <93683580+blueishegg@users.noreply.github.com>
2022-11-21 17:37:12 +0000

    navbar and photo grid complete

---
+++ src/components/profile-card/Profile.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Profile.css";
+
+export default function Profile({ photo, name }) {
+  return (
+    <div className="profileContainer">
+      <div className="nameTag">
+        <h2>{name}</h2>
+      </div>
+      <img src={photo} alt="Israel Calderon" className="profilePic" />
+      <div>
+        <p>
+          The first born and creator of this project. The objective of this
+          platform is to showcase my abilities as a software engineer and a
+          place to celebrate our family’s journey so far.
+        </p>
+        <p>Age. 31</p>
+        <p>Tennis in the summer and failing to ski in the winter.</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
-
+import "./Home.css";
+import Navbar from "../components/navbar/Navbar";
+import PhotoGrid from "../components/photo-grid/PhotoGrid";
+import Profile from "../components/profile-card/Profile";
 export default function Home() {
-  return <div>This is where the homepage will be</div>;
+  const israelProfile = "https://i.ibb.co/cYhxyxZ/b-w-profile.jpg";
+  const israelName = "Israel Calderon";
+  return (
+    <div className="homepage">
+      <Navbar />
+      <PhotoGrid />
+      <h1>MEET THE FAMILY</h1>
+      <div className="homepage-profiles">
+        <Profile photo={israelProfile} name={israelName} />
+      </div>
+    </div>
+  );
 }
